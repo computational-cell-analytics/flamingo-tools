59f450ac0fd6bce4e1eeffb7fb1f9450c2409d41
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-02-10 20:36:27 +0100

    Update syn marker visualization script

--- scripts/synapse_marker_detection/check_synapse_prediction.py
+++ scripts/synapse_marker_detection/check_synapse_prediction.py
@@ -1,5 +1,7 @@
 import h5py
+import napari
 import zarr
+
 from torch_em.util import load_model
 from torch_em.util.prediction import predict_with_halo
 from train_synapse_detection import get_paths
@@ -16,9 +18,18 @@ def run_prediction(val_image):
 def main():
     val_paths, _ = get_paths("val")
     val_image = zarr.open(val_paths[0])["raw"][:]
-    pred = run_prediction(val_image)
-    with h5py.File("pred.h5", "a") as f:
-        f.create_dataset("pred", data=pred, compression="gzip")
+
+    # pred = run_prediction(val_image)
+    # with h5py.File("pred.h5", "a") as f:
+    #     f.create_dataset("pred", data=pred, compression="gzip")
+
+    with h5py.File("pred.h5", "r") as f:
+        pred = f["pred"][:]
+
+    v = napari.Viewer()
+    v.add_image(val_image)
+    v.add_image(pred)
+    napari.run()
 
 
 if __name__ == "__main__":
