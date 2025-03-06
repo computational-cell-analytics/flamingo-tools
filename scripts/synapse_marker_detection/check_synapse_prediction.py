b788da7d22f213e72355491d036c67ee0243a47f
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-03-06 15:53:26 +0100

    Update prediction check

--- scripts/synapse_marker_detection/check_synapse_prediction.py
+++ scripts/synapse_marker_detection/check_synapse_prediction.py
@@ -1,7 +1,10 @@
 import h5py
 import napari
+import pandas as pd
 import zarr
 
+# from skimage.feature import blob_dog
+from skimage.feature import peak_local_max
 from torch_em.util import load_model
 from torch_em.util.prediction import predict_with_halo
 from train_synapse_detection import get_paths
@@ -16,8 +19,9 @@ def run_prediction(val_image):
 
 
 def main():
-    val_paths, _ = get_paths("val")
+    val_paths, val_labels = get_paths("val")
     val_image = zarr.open(val_paths[0])["raw"][:]
+    val_labels = pd.read_csv(val_labels[0])[["axis-0", "axis-1", "axis-2"]]
 
     # pred = run_prediction(val_image)
     # with h5py.File("pred.h5", "a") as f:
@@ -26,9 +30,17 @@ def main():
     with h5py.File("pred.h5", "r") as f:
         pred = f["pred"][:]
 
+    print("Running local max ...")
+    # coords = blob_dog(pred)
+    coords = peak_local_max(pred, min_distance=2, threshold_abs=0.2)
+    # breakpoint()
+    print("... done")
+
     v = napari.Viewer()
     v.add_image(val_image)
     v.add_image(pred)
+    v.add_points(coords)
+    v.add_points(val_labels, face_color="green")
     napari.run()
 
 
