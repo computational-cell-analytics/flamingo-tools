9d998f549258cb1dc368faa44dd6b22228e884e6
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-05-08 19:21:07 +0200

    Update the measurement implementation

--- flamingo_tools/measurements.py
+++ flamingo_tools/measurements.py
@@ -9,6 +9,7 @@ from skimage.measure import marching_cubes
 from tqdm import tqdm
 
 from .file_utils import read_image_data
+from .segmentation.postprocessing import _compute_table
 
 
 def _measure_volume_and_surface(mask, resolution):
@@ -25,35 +26,26 @@ def _measure_volume_and_surface(mask, resolution):
     return volume, surface
 
 
-# Could also support s3 directly?
-def compute_object_measures(
-    image_path: str,
-    segmentation_path: str,
-    segmentation_table_path: str,
-    output_table_path: str,
-    image_key: Optional[str] = None,
-    segmentation_key: Optional[str] = None,
+def compute_object_measures_impl(
+    image: np.typing.ArrayLike,
+    segmentation: np.typing.ArrayLike,
     n_threads: Optional[int] = None,
     resolution: float = 0.38,
-):
-    """
+    table: Optional[pd.DataFrame] = None,
+) -> pd.DataFrame:
+    """Compute simple intensity and morphology measures for each segmented cell in a segmentation.
+
+    See `compute_object_measures` for details.
 
     Args:
-        image_path:
-        segmentation_path:
-        segmentation_table_path:
-        output_table_path:
-        image_key:
-        segmentation_key:
-        n_threads:
-        resolution:
+        image: The image data.
+        segmentation: The segmentation.
+        n_threads: The number of threads to use for computation.
+        resolution: The resolution / voxel size of the data.
+        table: The segmentation table. Will be computed on the fly if it is not given.
     """
-    # First, we load the pre-computed segmentation table from MoBIE.
-    table = pd.read_csv(segmentation_table_path, sep="\t")
-
-    # Then, open the volumes.
-    image = read_image_data(image_path, image_key)
-    segmentation = read_image_data(segmentation_path, segmentation_key)
+    if table is None:
+        table = _compute_table(segmentation, resolution)
 
     def intensity_measures(seg_id):
         # Get the bounding box.
@@ -107,4 +99,44 @@ def compute_object_measures(
     # Create the result table and save it.
     keys = measures[0].keys()
     measures = pd.DataFrame({k: [measure[k] for measure in measures] for k in keys})
+    return measures
+
+
+# Could also support s3 directly?
+def compute_object_measures(
+    image_path: str,
+    segmentation_path: str,
+    segmentation_table_path: str,
+    output_table_path: str,
+    image_key: Optional[str] = None,
+    segmentation_key: Optional[str] = None,
+    n_threads: Optional[int] = None,
+    resolution: float = 0.38,
+) -> None:
+    """Compute simple intensity and morphology measures for each segmented cell in a segmentation.
+
+    This computes the mean, standard deviation, minimum, maximum, median and
+    5th, 10th, 25th, 75th, 90th and 95th percentile of the intensity image
+    per cell, as well as the volume and surface.
+
+    Args:
+        image_path: The filepath to the image data. Either a tif or hdf5/zarr/n5 file.
+        segmentation_path: The filepath to the segmentation data. Either a tif or hdf5/zarr/n5 file.
+        segmentation_table_path: The path to the segmentation table in MoBIE format.
+        output_table_path: The path for saving the segmentation with intensity measures.
+        image_key: The key (= internal path) for the image data. Not needed fir tif.
+        segmentation_key: The key (= internal path) for the segmentation data. Not needed for tif.
+        n_threads: The number of threads to use for computation.
+        resolution: The resolution / voxel size of the data.
+    """
+    # First, we load the pre-computed segmentation table from MoBIE.
+    table = pd.read_csv(segmentation_table_path, sep="\t")
+
+    # Then, open the volumes.
+    image = read_image_data(image_path, image_key)
+    segmentation = read_image_data(segmentation_path, segmentation_key)
+
+    measures = compute_object_measures_impl(
+        image, segmentation, n_threads, resolution, table=table
+    )
     measures.to_csv(output_table_path, sep="\t", index=False)
