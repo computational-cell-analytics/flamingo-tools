6464abd6e925e43d536edb2e6f4e1fe2f2e85203
anwai98 <anwai.archit@gmail.com>
2025-07-17 11:15:41 +0200

    First attempt to get CZI zebrafish data

---
+++ development/prepare_czi_zebrafish_data.py
@@ -0,0 +1,25 @@
+def get_czi_zebrafish_data():
+    # NOTE: Let's try for one link first, we can generalize it later.
+    url = "https://public.czbiohub.org/royerlab/ultrack/zebrafish_embryo.ome.zarr"
+
+    from ome_zarr.reader import Reader
+    from ome_zarr.io import parse_url
+
+    reader = Reader(parse_url(url))  # Prepare a reader.
+    nodes = list(reader())  # Might include multiple stuff
+    image_node = nodes[0]  # First node is expecte to be image pixel data.
+
+    dask_data = image_node.data  # Get the daskified data.
+
+    # HACK: Try it for one dask array with lowest resolution.
+    curr_data = dask_data[-1]
+
+    breakpoint()
+
+
+def main():
+    get_czi_zebrafish_data()
+
+
+if __name__ == "__main__":
+    main()
