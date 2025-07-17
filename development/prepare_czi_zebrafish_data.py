794e46ca3a8f602d20175812da5533f104753463
Anwai Archit <anwai.archit@gmail.com>
2025-07-17 16:17:22 +0200

    Finalize data loading for multiple resolutions

--- development/prepare_czi_zebrafish_data.py
+++ development/prepare_czi_zebrafish_data.py
@@ -1,9 +1,21 @@
-def get_czi_zebrafish_data():
+from ome_zarr.io import parse_url
+from ome_zarr.reader import Reader
+
+import dask.array as da
+
+
+def get_czi_zebrafish_data(view: bool = False) -> da.Array:
+    """Gets the CZI ZebraFish light-sheet microscopy data.
+    NOTE: Currently, we support only the raw data.
+
+    Args:
+        view: Whether to view the dask array via napari.
+
+    Returns:
+        The daskified chunky array.
+    """
     # NOTE: Let's try for one link first, we can generalize it later.
     url = "https://public.czbiohub.org/royerlab/ultrack/zebrafish_embryo.ome.zarr"
-
-    from ome_zarr.reader import Reader
-    from ome_zarr.io import parse_url
 
     reader = Reader(parse_url(url))  # Prepare a reader.
     nodes = list(reader())  # Might include multiple stuff
@@ -11,17 +23,27 @@ def get_czi_zebrafish_data():
 
     dask_data = image_node.data  # Get the daskified data.
 
-    # HACK: Try it for one dask array with lowest resolution.
-    curr_data = dask_data[-1]
+    # HACK: Try it for one dask array with lowest resolution (there exists four resolutions in this data).
+    curr_data = dask_data[-1]  # TODO: Control dimensions from here, the highest res starts at the first index.
 
-    # Load using napari
-    import napari
-    viewer = napari.view_image(curr_data, channel_axis=0)
-    napari.run()
+    # We don't care about the over-time information. Let's get the 3d info for now!
+    # I am removing the channel dimension here (OG dimension style: (T, C, Z, Y, X))
+    curr_data = curr_data[:, 0]  # TODO: Parse values in the time or z-dimension to parse limited slices?
+
+    # NOTE: The following line of code brings the entire dask array in memory.
+    # curr_data = curr_data.compute()
+
+    if view:
+        import napari
+        napari.view_image(curr_data)
+        napari.run()
+
+    return curr_data
 
 
 def main():
-    get_czi_zebrafish_data()
+    image = get_czi_zebrafish_data(view=False)
+    print(image.shape)
 
 
 if __name__ == "__main__":
