d6dbca49785fce4ca9408753f22afdaf527b9c99
Martin Schilling <martin.schilling@med.uni-goettingen.de>
2025-07-24 15:14:12 +0200

    Postprocess IHC segmentation based on high synapse count

---
+++ scripts/prediction/postprocess_ihc_synapse.py
@@ -0,0 +1,73 @@
+"""This script post-processes IHC segmentation with too many synapses based on a base segmentation and a reference.
+"""
+import argparse
+
+import imageio.v3 as imageio
+import pandas as pd
+import zarr
+from elf.io import open_file
+import flamingo_tools.segmentation.ihc_synapse_postprocessing as ihc_synapse_postprocessing
+from flamingo_tools.file_utils import read_image_data
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script to postprocess IHC segmentation based on the number of synapses per IHC.")
+
+    parser.add_argument('--base_path', type=str, required=True, help="Base segmentation.")
+    parser.add_argument('--ref_path', type=str, required=True, help="Reference segmentation.")
+    parser.add_argument('--out_path', type=str, required=True, help="Output segmentation.")
+
+    parser.add_argument('--base_table', type=str, required=True, help="Synapse per IHC table of base segmentation.")
+
+    parser.add_argument("--base_key", type=str, default=None,
+                        help="Input key for data in base segmentation.")
+    parser.add_argument("--ref_key", type=str, default=None,
+                        help="Input key for data in reference segmentation.")
+    parser.add_argument("--out_key", type=str, default="segmentation",
+                        help="Input key for data in output file.")
+
+    parser.add_argument('-r', "--resolution", type=float, default=0.38, help="Resolution of input in micrometer.")
+    parser.add_argument("--tif", action="store_true", help="Store output as tif file.")
+    parser.add_argument("--crop", action="store_true", help="Process crop of original array.")
+
+    parser.add_argument("--s3", action="store_true", help="Use S3 bucket.")
+    parser.add_argument("--s3_credentials", type=str, default=None,
+                        help="Input file containing S3 credentials. "
+                        "Optional if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY were exported.")
+    parser.add_argument("--s3_bucket_name", type=str, default=None,
+                        help="S3 bucket name. Optional if BUCKET_NAME was exported.")
+    parser.add_argument("--s3_service_endpoint", type=str, default=None,
+                        help="S3 service endpoint. Optional if SERVICE_ENDPOINT was exported.")
+
+    args = parser.parse_args()
+
+    if args.base_key is None:
+        data_base_ = read_image_data(args.base_path, args.base_key)
+    else:
+        data_base_ = open_file(args.base_path, "a")[args.base_key]
+    data_ref_ = read_image_data(args.ref_path, args.ref_key)
+
+    with open(args.base_table, "r") as f:
+        table_base = pd.read_csv(f, sep="\t")
+
+    if args.crop:
+        output_ = ihc_synapse_postprocessing.postprocess_ihc_synapse_crop(
+            data_base_, data_ref_, table_base=table_base, synapse_limit=25, min_overlap=0.5,
+            )
+    else:
+        output_ = ihc_synapse_postprocessing.postprocess_ihc_synapse(
+            data_base_, data_ref_, table_base=table_base, synapse_limit=25, min_overlap=0.5,
+            resolution=0.38, roi_buffer=40,
+            )
+
+    if args.tif:
+        imageio.imwrite(args.out_path, output_, compression="zlib")
+    else:
+        with zarr.open(args.out_path, mode="a") as f_out:
+            f_out.create_dataset(args.out_key, data=output_, compression="gzip")
+
+
+if __name__ == "__main__":
+
+    main()
