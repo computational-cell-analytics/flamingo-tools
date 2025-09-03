2d929c4b3b02eac152dfe8835d7d93fdd416e40f
Martin Schilling <martin.schilling@med.uni-goettingen.de>
2025-09-03 12:48:42 +0200

    Fixed output path

--- scripts/la-vision/postprocess_stardist.py
+++ scripts/la-vision/postprocess_stardist.py
@@ -17,12 +17,18 @@ def segment_with_stardist(ff, out):
 
 
 def main():
-    files = glob("predictions/sgn-new/*.tif")
-    out_folder = "./predictions/stardist"
+    direc = "/mnt/vast-nhr/projects/nim00007/data/moser/predictions/sgn-new"
+    out_folder = "/mnt/vast-nhr/projects/nim00007/data/moser/cochlea-lightsheet/predictions/stardist"
+
+    files = [entry.path for entry in os.scandir(direc) if ".tif" in entry.name]
+    files.sort()
+    file_names = [entry.name.split(".tif")[0] for entry in os.scandir(direc) if ".tif" in entry.name]
+    file_names.sort()
+
     os.makedirs(out_folder, exist_ok=True)
-    for ff in files:
-        out = imread(ff)
-        segment_with_stardist(ff, out)
+    for f_path, f_name in zip(files, file_names):
+        out = os.path.join(out_folder, f"{f_name}_seg.tif")
+        segment_with_stardist(f_path, out)
 
 
 if __name__ == "__main__":
